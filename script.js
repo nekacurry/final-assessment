f50b726ac4ba93333f389a53055d15a3ed61b8b6
nekacurry <anneka498@gmail.com>
2020-10-06 19:40:37 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,55 @@
+const display = document.querySelector('#display')
+const inputSize = document.querySelector('#input-size')
+const showSize = document.querySelector('#show-size')
+const inputFont = document.querySelector('#select-font')
+const showFont = document.querySelector('#show-font')
+const inputColor = document.querySelector('#input-color')
+const showColor = document.querySelector("#show-color")
+const inputBg = document.querySelector("input-bg-color")
+const showBg = document.querySelector("show-bg-color")
+
+
+
+
+function handleInput() {
+	// get the font size:
+	const fontSize = inputSize.value + 'px'
+	// Set the style 
+	display.style.fontSize = fontSize
+	// Show the size
+	showSize.innerHTML = fontSize
+}
+
+function handleFont() {
+    const fontType = inputFont.value
+
+    display.style.fontFamily = fontType
+
+    showFont.innerHTML = fontType
+}
+
+function handleColor() {
+    const fontColor = inputColor.value
+
+    display.style.color = fontColor
+
+    showColor.innerHTML = fontColor
+}
+
+function handleBG() {
+    const bgColor = inputBg.value
+
+    display.style.backgroundColor = bgColor
+
+    showBg.innerHTML = bgColor
+}
+
+function replace_text() {
+    var text = document.getElementById("enter-text").value
+    display.innerHTML = text
+}
+
+inputSize.addEventListener('input', handleInput)
+inputFont.addEventListener('input', handleFont)
+inputColor.addEventListener('input', handleColor)
+inputBg.addEventListener('input', handleBG)
